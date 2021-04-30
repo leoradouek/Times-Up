1f297c6359214ac9b15dc2e3e1b23f3a26b94236
Leora Douek <leorakurtz@gmail.com>
2021-04-30 15:08:56 -0400

    fix: heroku

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
+import Home from "./components/Homepage";
 import HowToPlay from "./components/HowToPlay";
 import Play from "./components/Play";
 import { me } from "./store";
